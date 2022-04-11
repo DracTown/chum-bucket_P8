ffed0500db0f983e548a21e932a15844d8922ce1
Spc-Drac <79214375+DracTown@users.noreply.github.com>
2022-04-11 17:16:56 -0400

    example help

--- pythonBeautifulSoup-main/ouline.py
+++ pythonBeautifulSoup-main/ouline.py
@@ -7,25 +7,38 @@ def displayJobDetails():
     print("Display job details")
 
 #function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
-def getJobList(role,location):
-   
-    # Complete the missing part of this function here 
+def getJobList(role, location):
+    # Complete the missing part of this function here
+    url = "https://www.indeed.com/jobs?q=" + role + "&l=" + location
 
-    url = "https://www.indeed.com/jobs?q="+ role + "&l=" + location
-
-    payload={}
+    payload = {}
     headers = {
-  'Cookie': 'CTK=1g0crhisekugl800; INDEED_CSRF_TOKEN=H1dD6yjOnmqLbnGqzb4t1a4sPlqW24Bu; JSESSIONID=65BE99942E4DF1EFC32EE45594493F11; PREF="TM=1649698982807:L=Charlotte"; RQ="q=+Software+Developer+&l=+Charlotte&ts=1649698982831"; UD="LA=1649698982:CV=1649698982:TS=1649698982:SG=56e8f01539738038954d8124c56dd2be"; ctkgen=1; indeed_rcc=""; jaSerpCount=1'
-}
-    
-   response = requests.request("GET", url, headers=headers, data=payload)
-    soup=BeautifulSoup(response.text,'html.parser')
-    jobTitle = soup.find('h2', class_='jobTitle').text
-    companyName = soup.find('span', class_='companyName').text
-    jobDescription = soup.find('div', class_='job-snippet').text
-    salary = soup.find('div', class_='salary-snippet-container').text
-    jobs = [jobTitle, companyName, jobDescription, salary]
-    print(jobs)
+            'Cookie': 'CTK=1fvqtabopq05p800; INDEED_CSRF_TOKEN=zObWlBjnc0oLVAS3aidx8lvSAZOgVukr; JSESSIONID=627D0DD3248349B13DE876D0883448E6; PREF="TM=1649096863522:L=Charlotte"; RQ="q=Software+Developer&l=+Charlotte&ts=1649703016594:q=+Software+Developer+&l=+Charlotte&ts=1649699917805&pts=1649129121573"; UD="LA=1649703016:LV=1649127002:CV=1649698419:TS=1649096863:SG=8138e1e8a2add6bf3bc2dced46849fdb"; indeed_rcc="PREF:CTK:UD:RQ"; jaSerpCount=2'
+         }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    jobTitle=soup.find_all('h2',attrs={'class':'jobTitle'})
+    companyName = soup.find_all('span', attrs={'class':'companyName'})
+    jobDescription = soup.find_all('div', attrs={'class':'job-snippet'})
+    salary = soup.find_all('div', attrs={'class':'salary-snippet-container'})
+    for title in jobTitle:
+        print(title.text)
+    for name in companyName:
+        print(name.text)
+    for des in jobDescription:
+        print(des.text)
+    for sal in salary:
+        print(sal.text)
+        
+
+
+    # for job in soup:
+    #  jobTitle = job.find_next('h2', class_='jobTitle')
+    #  companyName = job.find_next('span', class_='companyName')
+    #  jobDescription = job.find_next('div', class_='job-snippet')
+    #  salary = job.find_next('div', class_='salary-snippet-container')
+    #  jobs = [jobTitle, companyName, jobDescription, salary]
+    #  print(jobs)
 
     
 #save data in JSON file
