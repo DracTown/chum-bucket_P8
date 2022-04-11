7576c718c14797ac47467bb97847515b3052cac4
sjohn368 <sjohn368@uncc.edu>
2022-04-11 14:33:57 -0400

    oops

--- pythonBeautifulSoup-main/ouline.py
+++ pythonBeautifulSoup-main/ouline.py
@@ -10,22 +10,21 @@ def displayJobDetails():
 def getJobList(role,location):
     url = 'https://www.indeed.com/jobs?q={role}&l={location}'
     # Complete the missing part of this function here 
-    url = "https://www.indeed.com/jobs?q= Software Developer &l= Charlotte"
+
+    url = "https://www.indeed.com/jobs?q="+ role + "&l=" + location
 
     payload={}
     headers = {
-    'Cookie': 'CTK=1g0crhisekugl800; INDEED_CSRF_TOKEN=H1dD6yjOnmqLbnGqzb4t1a4sPlqW24Bu; JSESSIONID=65BE99942E4DF1EFC32EE45594493F11; PREF="TM=1649698982807:L=Charlotte"; RQ="q=+Software+Developer+&l=+Charlotte&ts=1649698982831"; UD="LA=1649698982:CV=1649698982:TS=1649698982:SG=56e8f01539738038954d8124c56dd2be"; ctkgen=1; indeed_rcc=""; jaSerpCount=1'
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    print(response.text)
+  'Cookie': 'CTK=1g0crhisekugl800; INDEED_CSRF_TOKEN=H1dD6yjOnmqLbnGqzb4t1a4sPlqW24Bu; JSESSIONID=65BE99942E4DF1EFC32EE45594493F11; PREF="TM=1649698982807:L=Charlotte"; RQ="q=+Software+Developer+&l=+Charlotte&ts=1649698982831"; UD="LA=1649698982:CV=1649698982:TS=1649698982:SG=56e8f01539738038954d8124c56dd2be"; ctkgen=1; indeed_rcc=""; jaSerpCount=1'
+}
     
     jobTitle = url.find('h2',class_='jobTitle').text
     companyName= url.find('span',class_='companyName').text
     jobDescription = url.find('div',class_='job-snippet').text
     salary = url.find('div',class_='salary-snippet-container').text
     
+    response = requests.request("GET", url, headers=headers, data=payload)
+
     jobs = [jobTitle, companyName, jobDescription, salary]
     
     print(jobs)
@@ -43,7 +42,6 @@ def main():
     role = input()
     # Complete the missing part of this function here
     print("Enter the Location to serch")
-    location=input()
-    getJobList(role, location)    
+    location=input()    
 if __name__ == '__main__':
     main()
