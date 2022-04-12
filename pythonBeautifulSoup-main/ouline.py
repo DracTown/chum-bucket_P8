d8d52c3cdc9e375e8ffefe2a2638142971e860d7
Spc-Drac <79214375+DracTown@users.noreply.github.com>
2022-04-12 17:12:38 -0400

    fixed small error were salary was adding a salary when there was none

--- pythonBeautifulSoup-main/ouline.py
+++ pythonBeautifulSoup-main/ouline.py
@@ -8,29 +8,26 @@ def displayJobDetails():
 jobResults = []
 def getJobList(role, location):
     # Complete the missing part of this function here
+    jobResults.clear()
     url = "https://www.indeed.com/jobs?q=" + role + "&l=" + location
     payload = {}
-     headers = {
+    headers = {
         'Cookie': 'CTK=1fvqtabopq05p800; INDEED_CSRF_TOKEN=zObWlBjnc0oLVAS3aidx8lvSAZOgVukr; JSESSIONID=627D0DD3248349B13DE876D0883448E6; PREF="TM=1649096863522:L=' + location + '"; RQ="q=' + role + '&l=+' + location + '&ts=1649703016594:q=+' + role + '+&l=+' + location + '&ts=1649699917805&pts=1649129121573"; UD="LA=1649703016:LV=1649127002:CV=1649698419:TS=1649096863:SG=8138e1e8a2add6bf3bc2dced46849fdb"; indeed_rcc="PREF:CTK:UD:RQ"; jaSerpCount=2'
     }
     response = requests.request("GET", url, headers=headers, data=payload)
     soup = BeautifulSoup(response.text, 'html.parser')
     for job in soup.find_all('div', attrs={'class': 'slider_container css-11g4k3a eu4oa1w0'}):
-        jobTitle = job.find_next('h2', class_='jobTitle')
-        companyName = job.find_next('span', class_='companyName')
-        jobDescription = job.find_next('div', class_='job-snippet')
-        salary = job.find_next('div', class_='salary-snippet-container')
+        jobTitle = job.find('h2', class_='jobTitle')
+        companyName = job.find('span', class_='companyName')
+        jobDescription = job.find('div', class_='job-snippet')
+        salary = job.find('div', class_='salary-snippet-container')
         if salary:
-            salary=salary.text
+            salary = salary.text
         else:
-         salary="None listed"
+            salary = "None listed"
+            
         jobs = [jobTitle.text, companyName.text, jobDescription.text, salary]
         jobResults.append(jobs)
-
-   def saveDataInJSON(jobDetails):
-    # Complete the missing part of this function here
-   
-    print("Saving data to JSON")
     
     
 #main function
